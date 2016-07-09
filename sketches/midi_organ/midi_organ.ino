1a857f7e9401f8d84e04b8ed07e86c3a8d7b4e82
Joshua Warner <silvertrumpet999@gmail.com>
2016-07-08 22:54:42 -0500

    FEAT: Working two-manual, 32 AGO pedalboard with software MIDI.

--- sketches/midi_organ/midi_organ.ino
+++ sketches/midi_organ/midi_organ.ino
@@ -24,7 +24,8 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 // constants won't change. They're used here to
 // set pin numbers for first 2 octaves
 const int keyPins[] = {28, 26, 24, 22, 13, 12, 11, 10, 9, 8, 7, 6, 14, 15, 16, 17, 18, 19, 20, 21, 27, 25, 23, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30,};
-const int rankPins[] = {5, }; // drive wire(s) for ranks
+const int rankPins[] = {5, 4, }; // drive wire(s) for ranks are: {5, 4, 3, 2, 67, and 69 (pedals)}
+const int pedalPin = 69;
 /*
   Note for reference, the analog -> digital pin mappings for the Mega are:
   A0 = 54
@@ -48,19 +49,20 @@ const int rankPins[] = {5, }; // drive wire(s) for ranks
 // define how large some things are (matrix size)
 const int numKeys = 61;    // full organ manual, also length of keyPins and midiKeyNotes
 const int numPedals = 32;  // full AGO 32-key pedalboard
-const int numManuals = 1;  // number of manuals
+const int numManuals = 2;  // number of manuals
 
 const unsigned char midiKeyNotes[numKeys] = {36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, };
-const unsigned char midiChannels[numManuals + 1] = {1, 16, };  // Five manuals plus pedals (pedals always rank 16)
-const int pedalChannel = numManuals;  // Zero-indexed so this is the final entry (pedals)
+const unsigned char midiChannels[numManuals] = {1, 2, };  // Five manuals plus pedals (pedals always rank 16)
+const unsigned char pedalChannel = 16;
 
 // State of all keys in a 2D multidimensional array
 bool keyStates[numManuals][numKeys] = {
   {no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, },
+  {no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, },
 };
 
 // State of all pedals in a 1D array
-bool pedalStates[numPedals] = {no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, }
+bool pedalStates[numPedals] = {no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, no, };
 
 
 // Convenience func to convert a boolean to the proper MIDI key velocity
@@ -92,11 +94,20 @@ void keyChange(bool value, bool &state, int key, int channel) {
 
 
 // function to handle the loop over a given rank, updating states
-void rankPoll(const int keyPins[], bool (&states)[], int arraySize, int rank) {
+void keyPoll(const int keyPins[], bool (&states)[numKeys], int arraySize, int rank) {
   for(int key=0; key<arraySize; key++){
       // run through all switches, reading their values
       // pass results to keyChange which will dispatch a MIDI event if needed
       keyChange(digitalRead(keyPins[key]), states[key], key, midiChannels[rank]);
+  }
+}
+
+// function to loop over pedals
+void pedalPoll(const int pollPin, bool (&states)[numPedals], int arraySize, int pedalMidiChannel) {
+  for(int key=0; key<arraySize; key++){
+      // run through all switches, reading their values
+      // pass results to keyChange which will dispatch a MIDI event if needed
+      keyChange(digitalRead(pollPin), states[key], key, pedalMidiChannel);
   }
 }
 
@@ -125,7 +136,7 @@ void loop() {
       digitalWrite(rankPins[manual], LOW);
 
       // poll all keys, sending MIDI events if warranted
-      rankPoll(keyPins, keyStates[manual], numKeys, manual);
+      keyPoll(keyPins, keyStates[manual], numKeys, manual);
 
       // return the drive pin to +5V (off)
       digitalWrite(rankPins[manual], HIGH);
@@ -133,6 +144,6 @@ void loop() {
 
   // read the pedals
   digitalWrite(pedalPin, LOW);
-  rankPoll(pedalPins, pedalStates, numPedals, pedalChannel);
+  pedalPoll(pedalPin, pedalStates, numPedals, pedalChannel);
   digitalWrite(pedalPin, HIGH);
 }
